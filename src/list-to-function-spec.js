13f01f3b70069bb92138d5c10067ac05d4fc4e30
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-11-03 10:03:03 -0400

    initial code

---
+++ src/list-to-function-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const listToFunction = require('.')
+
+describe('list-to-function', () => {
+  it('write this test', () => {
+    console.assert(listToFunction, 'should export something')
+  })
+})
